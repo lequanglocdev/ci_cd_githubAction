c47e9972ecfa64225e140eec3822bb2589838e9a
lequanglocdev <quanglocdev@gmail.com>
2024-09-19 10:45:31 +0700

    fix

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,5 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <h1>Slug: {params.slug} <p>QuangLoc</p> <p>TPHCM</p><p>quanglocdev@gmail.com</p></h1>;
+  return <h1>Slug: {params.slug} <p>Quang Loc</p></h1>;
 }
